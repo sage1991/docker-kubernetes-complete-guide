a4946362d210f5076f127de667176c01f17db529
Harry <harry.kane@kakaostyle.com>
2023-01-05 15:14:01 +0900

    Add title at client

--- complex/complex-client/src/App.tsx
+++ complex/complex-client/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Fib calculator</h1>
         <RootRouter />
       </header>
     </div>
